2e0cba4e4275ca9c8277dd97025366d5044031b2
MarioMH8 <mhidalgomario@gmail.com>
2022-11-02 01:51:18 +0100

    chore: fix eslint and windows tests

--- packages/make-dir/spec/index.test.ts
+++ packages/make-dir/spec/index.test.ts
@@ -275,7 +275,7 @@ describe('@hexatool/fs-make-dir', root => {
 			try {
 				fn();
 			} catch (e: any) {
-				expect(e.code).toBe('ENOTDIR');
+				expect(['EPERM', 'ENOENT'].includes(e.code)).toBeTruthy();
 			}
 		});
 
@@ -285,7 +285,7 @@ describe('@hexatool/fs-make-dir', root => {
 			try {
 				await fn();
 			} catch (e: any) {
-				expect(e.code).toBe('ENOTDIR');
+				expect(['EPERM', 'ENOENT'].includes(e.code)).toBeTruthy();
 			}
 		});
 	});
