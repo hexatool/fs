875dd9b332eec1e27d3afa32cab0cfc4e5a3b030
MarioMH8 <mhidalgomario@gmail.com>
2022-11-16 14:19:56 +0100

    fix: corregir vite.config.ts

--- packages/empty-dir/vite.config.ts
+++ packages/empty-dir/vite.config.ts
@@ -1,4 +1,4 @@
-import path, { resolve } from 'node:path';
+import { resolve } from 'node:path';
 
 import typescript2 from 'rollup-plugin-typescript2';
 import { defineConfig } from 'vite';
@@ -17,11 +17,20 @@ export default defineConfig({
 		rollupOptions: {
 			external: [
 				'node:fs',
+				'node:fs/promises',
 				'node:path',
-				'@hexatool/fs-remove/async',
+				'@hexatool/fs-create-file/async',
+				// '@hexatool/fs-empty-dir/async',
+				'@hexatool/fs/async',
 				'@hexatool/fs-make-dir/async',
-				'@hexatool/fs-remove',
+				'@hexatool/fs-remove/async',
+				'@hexatool/fs-stat/async',
+				'@hexatool/fs-create-file',
+				// '@hexatool/fs-empty-dir',
+				'@hexatool/fs',
 				'@hexatool/fs-make-dir',
+				'@hexatool/fs-remove',
+				'@hexatool/fs-stat',
 			],
 			output: {
 				exports: 'named',
@@ -30,14 +39,19 @@ export default defineConfig({
 	},
 	resolve: {
 		alias: {
-			'@hexatool/fs/async': path.resolve(__dirname, '../fs/src/async'),
-			'@hexatool/fs-remove/async': path.resolve(__dirname, '../remove/src/async'),
-			'@hexatool/fs-empty-dir/async': path.resolve(__dirname, '../empty-dir/src/async'),
-			'@hexatool/fs-make-dir/async': path.resolve(__dirname, '../make-dir/src/async'),
-			'@hexatool/fs': path.resolve(__dirname, '../fs/src/sync'),
-			'@hexatool/fs-remove': path.resolve(__dirname, '../remove/src/sync'),
-			'@hexatool/fs-empty-dir': path.resolve(__dirname, '../empty-dir/src/sync'),
-			'@hexatool/fs-make-dir': path.resolve(__dirname, '../make-dir/src/sync'),
+			'@hexatool/fs-create-file/async': resolve(__dirname, '../packages/create-file/src/async'),
+			// '@hexatool/fs-empty-dir/async': resolve(__dirname, '../packages/empty-dir/src/async'),
+			'@hexatool/fs/async': resolve(__dirname, '../packages/fs/src/async'),
+			'@hexatool/fs-make-dir/async': resolve(__dirname, '../packages/make-dir/src/async'),
+			'@hexatool/fs-remove/async': resolve(__dirname, '../packages/remove/src/async'),
+			'@hexatool/fs-stat/async': resolve(__dirname, '../packages/stat/src/async'),
+
+			'@hexatool/fs-create-file': resolve(__dirname, '../packages/create-file/src/sync'),
+			// '@hexatool/fs-empty-dir': resolve(__dirname, '../packages/empty-dir/src/sync'),
+			'@hexatool/fs': resolve(__dirname, '../packages/fs/src/sync'),
+			'@hexatool/fs-make-dir': resolve(__dirname, '../packages/make-dir/src/sync'),
+			'@hexatool/fs-remove': resolve(__dirname, '../packages/remove/src/sync'),
+			'@hexatool/fs-stat': resolve(__dirname, '../packages/stat/src/sync'),
 		},
 	},
 	plugins: [
--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,20 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	resolve: {
 		alias: {
-º			'@hexatool/fs/async': path.resolve(__dirname, './packages/fs/src/async'),
-			'@hexatool/fs-remove/async': path.resolve(__dirname, './packages/remove/src/async'),
+			'@hexatool/create-file/async': path.resolve(__dirname, './packages/create-file/src/async'),
 			'@hexatool/fs-empty-dir/async': path.resolve(__dirname, './packages/empty-dir/src/async'),
+			'@hexatool/fs/async': path.resolve(__dirname, './packages/fs/src/async'),
 			'@hexatool/fs-make-dir/async': path.resolve(__dirname, './packages/make-dir/src/async'),
-			'@hexatool/fs': path.resolve(__dirname, './packages/fs/src/sync'),
-			'@hexatool/fs-remove': path.resolve(__dirname, './packages/remove/src/sync'),
+			'@hexatool/fs-remove/async': path.resolve(__dirname, './packages/remove/src/async'),
+			'@hexatool/fs-stat/async': path.resolve(__dirname, './packages/stat/src/async'),
+
+			'@hexatool/create-file': path.resolve(__dirname, './packages/create-file/src/sync'),
 			'@hexatool/fs-empty-dir': path.resolve(__dirname, './packages/empty-dir/src/sync'),
+			'@hexatool/fs': path.resolve(__dirname, './packages/fs/src/sync'),
 			'@hexatool/fs-make-dir': path.resolve(__dirname, './packages/make-dir/src/sync'),
+			'@hexatool/fs-remove': path.resolve(__dirname, './packages/remove/src/sync'),
+			'@hexatool/fs-stat': path.resolve(__dirname, './packages/stat/src/sync'),
 		},
 	},
 });
+
