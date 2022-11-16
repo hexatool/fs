1f0b029a60029394c64e9e262246dd22a0a6ca42
MarioMH8 <mhidalgomario@gmail.com>
2022-11-16 13:09:13 +0100

    feat: cambiar el nombre del repositorio raíz

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,13 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	resolve: {
 		alias: {
-			'@hexatool/fs-remove/async': path.resolve(__dirname, './packages/remove/src/sync'),
-			'@hexatool/fs-empty-dir/async': path.resolve(__dirname, './packages/make-dir/src/sync'),
-			'@hexatool/fs-make-dir/async': path.resolve(__dirname, './packages/make-dir/src/sync'),
+º			'@hexatool/fs/async': path.resolve(__dirname, './packages/fs/src/async'),
+			'@hexatool/fs-remove/async': path.resolve(__dirname, './packages/remove/src/async'),
+			'@hexatool/fs-empty-dir/async': path.resolve(__dirname, './packages/empty-dir/src/async'),
+			'@hexatool/fs-make-dir/async': path.resolve(__dirname, './packages/make-dir/src/async'),
+			'@hexatool/fs': path.resolve(__dirname, './packages/fs/src/sync'),
 			'@hexatool/fs-remove': path.resolve(__dirname, './packages/remove/src/sync'),
-			'@hexatool/fs-empty-dir': path.resolve(__dirname, './packages/make-dir/src/sync'),
+			'@hexatool/fs-empty-dir': path.resolve(__dirname, './packages/empty-dir/src/sync'),
 			'@hexatool/fs-make-dir': path.resolve(__dirname, './packages/make-dir/src/sync'),
 		},
 	},
