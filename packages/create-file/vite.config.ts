2f571e7ab055d7269278adbadaf82697b3d05ead
MarioMH8 <mhidalgomario@gmail.com>
2023-01-21 21:13:45 +0100

    chore: actualizar la configuración de Vite

--- packages/create-file/vite.config.ts
+++ packages/create-file/vite.config.ts
@@ -1,8 +1,10 @@
 import { resolve } from 'node:path';
 
-import typescript2 from 'rollup-plugin-typescript2';
-
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+import externalize from '../../vite.plugin.external';
 
 export default defineConfig({
 	build: {
@@ -15,83 +17,13 @@ export default defineConfig({
 		},
 		minify: false,
 		sourcemap: false,
-		rollupOptions: {
-			external: [
-				'node:fs',
-				'node:fs/promises',
-				'node:path',
-				'graceful-fs',
-				'@hexatool/fs-file-system',
-				'@hexatool/fs-copy/async',
-				// '@hexatool/fs-create-file/async',
-				'@hexatool/fs-create-link/async',
-				'@hexatool/fs-empty-dir/async',
-				'@hexatool/fs/async',
-				'@hexatool/fs-make-dir/async',
-				'@hexatool/fs-move/async',
-				'@hexatool/fs-path-exists/async',
-				'@hexatool/fs-remove/async',
-				'@hexatool/fs-stat/async',
-				'@hexatool/fs-copy',
-				// '@hexatool/fs-create-file',
-				'@hexatool/fs-create-link',
-				'@hexatool/fs-empty-dir',
-				'@hexatool/fs',
-				'@hexatool/fs-make-dir',
-				'@hexatool/fs-move',
-				'@hexatool/fs-path-exists',
-				'@hexatool/fs-remove',
-				'@hexatool/fs-stat',
-			],
-			output: {
-				exports: 'named',
-			},
-		},
 	},
-	resolve: {
-		alias: {
-			'@hexatool/fs-file-system': resolve(__dirname, '../file-system/src/index'),
-			'@hexatool/fs-copy/async': resolve(__dirname, '../copy/src/async'),
-			'@hexatool/fs-create-file/async': resolve(__dirname, './src/async'),
-			'@hexatool/fs-create-link/async': resolve(__dirname, '../create-link/src/async'),
-			'@hexatool/fs-empty-dir/async': resolve(__dirname, '../empty-dir/src/async'),
-			'@hexatool/fs/async': resolve(__dirname, '../fs/src/async'),
-			'@hexatool/fs-make-dir/async': resolve(__dirname, '../make-dir/src/async'),
-			'@hexatool/fs-move/async': resolve(__dirname, '../move/src/async'),
-			'@hexatool/fs-path-exists/async': resolve(__dirname, '../path-exists/src/async'),
-			'@hexatool/fs-remove/async': resolve(__dirname, '../remove/src/async'),
-			'@hexatool/fs-stat/async': resolve(__dirname, '../stat/src/async'),
-
-			'@hexatool/fs-copy': resolve(__dirname, '../copy/src/sync'),
-			'@hexatool/fs-create-file': resolve(__dirname, './src/sync'),
-			'@hexatool/fs-create-link': resolve(__dirname, '../create-link/src/sync'),
-			'@hexatool/fs-empty-dir': resolve(__dirname, '../empty-dir/src/sync'),
-			'@hexatool/fs': resolve(__dirname, '../fs/src/sync'),
-			'@hexatool/fs-make-dir': resolve(__dirname, '../make-dir/src/sync'),
-			'@hexatool/fs-move': resolve(__dirname, '../move/src/sync'),
-			'@hexatool/fs-path-exists': resolve(__dirname, '../path-exists/src/sync'),
-			'@hexatool/fs-remove': resolve(__dirname, '../remove/src/sync'),
-			'@hexatool/fs-stat': resolve(__dirname, '../stat/src/sync'),
-		},
-	},
-
 	plugins: [
-		{
-			...typescript2({
-				check: false,
-				tsconfigOverride: {
-					compilerOptions: {
-						module: 'ES2020',
-						declaration: true,
-						declarationDir: 'dist/types',
-						emitDeclarationOnly: true,
-						baseUrl: '.',
-					},
-				},
-				include: ['src/*.ts+(|x)', 'src/**/*.ts+(|x)'],
-				useTsconfigDeclarationDir: true,
-			}),
-			apply: 'build',
-		},
+		dts({
+			skipDiagnostics: true,
+			rollupTypes: true,
+		}),
+		externalize(),
+		tsconfigPaths(),
 	],
 });
