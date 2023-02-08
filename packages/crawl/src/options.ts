973f4ddf216a14a8b4977f770624c2a8bb675573
MarioMH8 <mhidalgomario@gmail.com>
2023-02-08 15:44:47 +0100

    feat: create specific options for up and down crawler

--- packages/crawl/src/options.ts
+++ packages/crawl/src/options.ts
@@ -1,19 +1,30 @@
 export type FilterPredicate = (path: string, isDirectory: boolean) => boolean;
 
 export type ExcludePredicate = (dirName: string, dirPath: string) => boolean;
+
 export type CrawlDirection = 'down' | 'up';
 
-export interface Options {
+export interface CommonCrawlerOptions {
 	direction: CrawlDirection;
 	exclude?: ExcludePredicate;
 	excludeFiles?: boolean;
 	filters?: FilterPredicate[];
 	includeBasePath?: boolean;
 	includeDirs?: boolean;
-	maxDepth?: number;
 	normalizePath?: boolean;
-	relativePaths?: boolean;
 	resolvePaths?: boolean;
-	resolveSymlinks?: boolean;
 	suppressErrors?: boolean;
 }
+
+export interface CrawlerDownOptions extends CommonCrawlerOptions {
+	direction: 'down';
+	maxDepth?: number;
+	relativePaths?: boolean;
+	resolveSymlinks?: boolean;
+}
+
+export interface CrawlerUpOptions extends CommonCrawlerOptions {
+	direction: 'up';
+}
+
+export type CrawlerOptions = CrawlerDownOptions | CrawlerUpOptions;
