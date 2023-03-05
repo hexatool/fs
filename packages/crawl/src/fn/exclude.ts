fb6e3efd0a32052250acc43fd8ccf6d6d91d1f02
MarioMH8 <mhidalgomario@gmail.com>
2023-03-06 00:28:47 +0100

    feat: extract excludeFn to own file

---
+++ packages/crawl/src/fn/exclude.ts
@@ -0,0 +1,51 @@
+import type { Dirent } from 'node:fs';
+
+import type { ExcludePathFn, ExcludeType } from '../types/options';
+import type { ReadDirFn } from './read-dir';
+
+function matchExcludeRegex(path: string, regex: RegExp): boolean {
+	return regex.test(path);
+}
+
+function matchExcludeString(path: string, term: string): boolean {
+	return path === term;
+}
+
+function matchExcludeFn(path: string, fn: ExcludePathFn): boolean {
+	return fn(path);
+}
+
+type ExcludeFn<Exclude extends ExcludeType> = (path: string, exclude: Exclude) => boolean;
+
+function matchExclude<Exclude extends ExcludeType>(ex: Exclude): ExcludeFn<Exclude> {
+	if (typeof ex === 'string') {
+		return matchExcludeString as ExcludeFn<Exclude>;
+	}
+	if (typeof ex === 'function') {
+		return matchExcludeFn as ExcludeFn<Exclude>;
+	}
+
+	return matchExcludeRegex as ExcludeFn<Exclude>;
+}
+
+export default function excludeFn<
+	Exclude extends ExcludeType,
+	Output extends Dirent | string,
+	Fn extends ReadDirFn<Output>
+>(readDir: Fn, exclude?: Exclude): Fn {
+	if (!exclude) {
+		return readDir;
+	}
+	const excludeFn = matchExclude(exclude);
+
+	return ((path: string, callback) => {
+		if (!excludeFn(path, exclude)) {
+			return readDir(path, callback);
+		}
+		if (callback) {
+			callback(null, []);
+		} else {
+			return [];
+		}
+	}) as Fn;
+}
--- packages/crawl/src/fn/read-dir.ts
+++ packages/crawl/src/fn/read-dir.ts
@@ -3,7 +3,8 @@ import type { Dirent } from 'node:fs';
 import { fs } from '@hexatool/fs-file-system';
 
 import type { CallBack, ResultType } from '../types';
-import type { ExcludePathFn, ExcludeType } from '../types/options';
+import type { ExcludeType } from '../types/options';
+import excludeFn from './exclude';
 
 export type CallbackReadDirFn<Output extends Dirent | string> = (
 	path: string,
@@ -21,53 +22,6 @@ const syncDirentReadFn: SyncReadDirFn<Dirent> = path =>
 const callBackReadFn: CallbackReadDirFn<string> = (path, callback) => fs.readdir(path, callback);
 
 const syncReadFn: SyncReadDirFn<string> = path => fs.readdirSync(path);
-
-function matchExcludeRegex(path: string, regex: RegExp): boolean {
-	return regex.test(path);
-}
-
-function matchExcludeString(path: string, term: string): boolean {
-	return path === term;
-}
-
-function matchExcludeFn(path: string, fn: ExcludePathFn): boolean {
-	return fn(path);
-}
-
-type ExcludeFn<Exclude extends ExcludeType> = (path: string, exclude: Exclude) => boolean;
-
-function matchExclude<Exclude extends ExcludeType>(ex: Exclude): ExcludeFn<Exclude> {
-	if (typeof ex === 'string') {
-		return matchExcludeString as ExcludeFn<Exclude>;
-	}
-	if (typeof ex === 'function') {
-		return matchExcludeFn as ExcludeFn<Exclude>;
-	}
-
-	return matchExcludeRegex as ExcludeFn<Exclude>;
-}
-
-function excludeFn<
-	Exclude extends ExcludeType,
-	Output extends Dirent | string,
-	Fn extends ReadDirFn<Output>
->(readDir: Fn, exclude?: Exclude): Fn {
-	if (!exclude) {
-		return readDir;
-	}
-	const excludeFn = matchExclude(exclude);
-
-	return ((path: string, callback) => {
-		if (!excludeFn(path, exclude)) {
-			return readDir(path, callback);
-		}
-		if (callback) {
-			callback(null, []);
-		} else {
-			return [];
-		}
-	}) as Fn;
-}
 
 export default function readDirFn(
 	api: 'callback',
